74042db2b3d335710d61e4df8d979cfe69d0a4ab
namseok <lsouth3n@gmail.com>
2024-07-24 15:32:55 +0900

    add updates

--- src/components/section/Header.jsx
+++ src/components/section/Header.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const header = () => {
   return (
-    <header id="header" role="banner">header</header>
+    <header id="header" role="banner">
+      <h1 className="header__logo">
+        <a href="/">
+          <em></em>
+          
+        </a>
+      </h1>
+      <div className="header__menu"></div>
+      <div className="header__sns"></div>
+    </header>
   )
 }
 
